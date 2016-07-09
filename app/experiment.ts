fda5c2f9f9021442f4a154ab08e91f057924a13b
Janikaka <janika.kaariainen@helsinki.fi>
2016-07-09 18:30:44 +0300

    fresh start

--- app/experiment.ts
+++
@@ -1,4 +0,0 @@
-export class Experiment {
-	id: number;
-	name: string;
-}
--- app/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-
-import { AppComponent } from './app.component';
-import { APP_ROUTER_PROVIDERS } from './app.routers';
-
-bootstrap(AppComponent, [
-  APP_ROUTER_PROVIDERS
-]);
