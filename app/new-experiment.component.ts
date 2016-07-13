46ae124dea2ebe5bc7ce3f838a9e9b66f4835a49
Janikaka <janika.kaariainen@helsinki.fi>
2016-07-13 22:15:07 +0300

    Create new experiment form fixed

--- app/new-experiment.component.ts
+++ app/new-experiment.component.ts
@@ -9,81 +9,39 @@ import { ExperimentServerService } from './experiment-server.service';
   templateUrl: 'app/new-experiment.component.html'
 })
 export class NewExperimentComponent {
-	
 	constructor(
     	private experimentServerService: ExperimentServerService) {
   	}
-
 	model = new Experiment(null, '', []);
 	submitted = false;
+	
 	experimentgroups = [];
-
-
+	addExperimentgroup() {
+		this.experimentgroups.push({'id': this.experimentgroups.length+1, 'name':'', 'configurations':[]});
+	}
+	addConf(id) {
+		this.experimentgroups[id-1]['configurations'].push({'id':this.experimentgroups[id-1]['configurations'].length+1, 'key':'', 'value': null});
+	}
 	onSubmit() { 
-		
 		this.submitted = true;
-		let expGroupElements = document.getElementsByClassName("experimentgroup");
-		let confKeyElements = document.getElementsByClassName("confKey");
-		let confValueElements = document.getElementsByClassName("confValue");
-
-
-		this.experimentgroups = [];
 		this.model.experimentgroups = [];
-
-		for(let i = 0; i < expGroupElements.length; i++) {
-			let expgroup = expGroupElements[i]['value'];
-			let confKey = confKeyElements[i]['value'];
-			let confValue = confValueElements[i]['value'];
-			this.experimentgroups.push({'name': expgroup, 'configurations': [{'key':confKey, 'value':confValue}]});
-			this.model.experimentgroups.push({'id':100, 'name': expgroup, 'configurations': [{'id':1001,'key':confKey, 'value':confValue}]});
+		for(let j = 1; j <= this.experimentgroups.length; j++) {
+			let name = document.getElementById(j)['value'];
+			let experimentgroup = {'id':null,'name': name, 'configurations': []};
+			for(let i = 1; i <= this.experimentgroups[j-1]['configurations'].length; i++) {
+				let key = document.getElementById(j + '_' + i +'_key')['value'];
+				let value = document.getElementById(j + '_' + i + '_value')['value'];
+				let conf = {'id':null, 'key':key, 'value':value};
+				experimentgroup['configurations'].push(conf);
+			}
+			this.model.experimentgroups.push(experimentgroup);
 		}
-		
+
 		this.experimentServerService.createExperiment(this.model);
 		
 	}
 
-	addExperimentgroup() {
-		let div = document.createElement("div");
-		let name = document.createTextNode("Name: ");
-		div.appendChild(name);
-		let nameInput = document.createElement("input");
-		nameInput.type = "text";
-		nameInput.placeholder = "Enter experiment group";
-		nameInput.className = "experimentgroup";
-		div.appendChild(nameInput);
-		let br = document.createElement("br");
-		div.appendChild(br);
-		div.appendChild(br);
-
-		let keyDiv = document.createElement("div");
-		let key = document.createTextNode("Conf key: ");
-		keyDiv.appendChild(key);
-		let confKeyInput = document.createElement("input");
-		confKeyInput.type = "text";
-		confKeyInput.placeholder = "Enter conf key";
-		confKeyInput.className = "confKey";
-		keyDiv.appendChild(confKeyInput);
-		keyDiv.appendChild(br);
-		keyDiv.appendChild(br);
-
-		let valueDiv = document.createElement("div");
-		let value = document.createTextNode("Conf value: ");
-		valueDiv.appendChild(value);
-		let confValueInput = document.createElement("input");
-		confValueInput.type = "text";
-		confValueInput.placeholder = "Enter conf value";
-		confValueInput.className = "confValue";
-		valueDiv.appendChild(confValueInput);
-		valueDiv.appendChild(br);
-
-		document.getElementById("experimentgroupDIV").appendChild(div);
-		document.getElementById("experimentgroupDIV").appendChild(keyDiv);
-		document.getElementById("experimentgroupDIV").appendChild(valueDiv);
-
-		let emptyDiv = document.createElement("div");
-		emptyDiv.appendChild(document.createElement("p"));
-		document.getElementById("experimentgroupDIV").appendChild(emptyDiv);
-	}
+	
 
 	active = true;
 }
