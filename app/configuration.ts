0ce492a13e8d7700c85eead3020a6cd33e3ba812
Janika Kääriäinen <janika.kaariainen@helsinki.fi>
2016-08-10 20:29:44 +0300

    type select for confs

--- app/configuration.ts
+++ app/configuration.ts
@@ -1,9 +1,9 @@
 export class Configuration {
 	id: number;
 	key: string;
-	value: number;
+	value: any;
 
-	constructor(id: number, key: string, value: number) {
+	constructor(id: number, key: string, value: any) {
 		this.id = id;
 		this.key = key;
 		this.value = value;
--- app/new-experiment.component.ts
+++ app/new-experiment.component.ts
@@ -10,6 +10,7 @@ import { ExperimentServerService } from './experiment-server.service';
   templateUrl: 'app/new-experiment.component.html'
 })
 export class NewExperimentComponent {
+	valueTypes = ['Integer', 'Double', 'Boolean', 'String'];
 	constructor(
     	private experimentServerService: ExperimentServerService) {
   	}
@@ -23,6 +24,10 @@ export class NewExperimentComponent {
 	addConf(id) {
 		this.experimentgroups[id-1]['configurations'].push({'id':this.experimentgroups[id-1]['configurations'].length+1, 'key':'', 'value': null});
 	}
+	setConfValueType(expgroupId, confId, type) {
+		console.log("setConfValueType: " + type);
+		this.experimentgroups[expgroupId-1]['configurations'][confId-1]['type'] = type;
+	}
 	onSubmit() { 
 		this.submitted = true;
 		this.model.experimentgroups = [];
@@ -32,6 +37,16 @@ export class NewExperimentComponent {
 			for(let i = 1; i <= this.experimentgroups[j-1]['configurations'].length; i++) {
 				let key = document.getElementById(j + '_' + i +'_key')['value'];
 				let value = document.getElementById(j + '_' + i + '_value')['value'];
+				let type = document.getElementById(j + '_' + i + '_select')['value'];
+				console.log("key: " + key + ", value: " + value + ", type: " + type);
+				if(type == 'Integer') {
+					value = parseInt(value);
+				} else if(type == 'Double') {
+					value = parseFloat(value); //Problem: 5.0 === 5
+				} else if(type == 'Boolean') {
+					value = Boolean(value);
+				}
+				console.log("typeof value: " + typeof(value));
 				let conf = {'id':null, 'key':key, 'value':value};
 				experimentgroup['configurations'].push(conf);
 			}
